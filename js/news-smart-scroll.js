08e919b821cabc3fec90b997fcb6bfded4ff6230
AleksandrHunko <59770725+AleksandrHunko@users.noreply.github.com>
2021-01-18 16:39:17 +0200

    Split javascript

---
+++ js/news-smart-scroll.js
@@ -0,0 +1,17 @@
+"use_strict"
+
+window.onload = function () {
+
+  if (document.documentElement.scrollTop < document.querySelector('.news-block').offsetTop) {
+    document.querySelector('.search-block').classList.remove('fixed')
+    document.querySelector('.news').classList.remove('align-bottom')
+  } else if (
+    document.documentElement.scrollTop > (document.querySelector('.news-block').offsetTop - 10) &&
+    document.documentElement.scrollTop < (document.querySelector('.news-block').offsetTop + document.querySelector('.news-block').offsetHeight - 656)
+  ) {
+    document.querySelector('.search-block').classList.add('fixed')
+  } else {
+    document.querySelector('.search-block').classList.remove('fixed')
+    document.querySelector('.news').classList.add('align-bottom')
+  }
+}
