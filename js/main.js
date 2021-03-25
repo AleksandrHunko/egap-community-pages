52b2dc84319b01746d616aafb106313be01e4a40
AleksandrHunko <59770725+AleksandrHunko@users.noreply.github.com>
2021-03-25 12:03:01 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,4 +16,18 @@ window.addEventListener('load', function () {
       document.body.style.overflowY = "auto";
     }
   })
+
+
+  const inputs = document.querySelectorAll(".stylyzed-input");
+  if (inputs.length > 0) {
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].querySelector('input').addEventListener('change', function() {
+        if (this.value == "") {
+          this.classList.remove('valid')
+        } else {
+          this.classList.add('valid')
+        }
+      })
+    }
+  }
 })
